3cd4a61ff5b27500699260bb0c1ee4de851ed8dc
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-11-30 09:10:09 -0600

    Merged with main

---
+++ src/Microsoft.Bot.Builder.Runtime/Builders/OnTurnError/IOnTurnErrorHandlerBuilder.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Runtime.Builders.OnTurnError
+{
+    public interface IOnTurnErrorHandlerBuilder : IBuilder<Func<ITurnContext, Exception, Task>>
+    {
+    }
+}

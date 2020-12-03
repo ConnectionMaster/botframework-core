db1c091632082ef4cdfa21b5907b2770927b086a
Ryan Lengel Isgrig <43043272+ryanlengel@users.noreply.github.com>
2020-12-02 20:10:09 -0800

    Fix build

--- tests/Microsoft.Bot.Builder.Runtime.Tests/Builders/Handlers/OnTurnErrorHandlerBuilderTests.cs
+++ tests/Microsoft.Bot.Builder.Runtime.Tests/Builders/Handlers/OnTurnErrorHandlerBuilderTests.cs
@@ -11,7 +11,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
-using Moq;
 using Xunit;
 
 namespace Microsoft.Bot.Builder.Runtime.Tests.Builders.Handlers
